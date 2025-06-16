b9593169a3cf943affe674e3ec9d4bb170cf3e82
Atishsankaje <atishsankaje@gmail.com>
2025-06-16 15:43:37 +0530

    hey commit

--- app/views.py
+++ app/views.py
@@ -9,8 +9,14 @@ def index(request):
     print(api_url)
     api=requests.get(api_url).json()
     temperature=api['main']['temp']
+    wind_speed = api['wind']['speed']
+    humidity = api['main']['humidity']
     cityname=api['name']
     country=api['sys']['country']
+    weather = api["weather"][0]['main']
+    description = api['weather'][0]['description']
+    icon = api['weather'][0]['icon']
+    img_url = f"https://openweathermap.org/img/wn/{icon}@2x.png"
 
-    return render(request,'index.html',{'temperature':temperature, 'cityname':cityname, 'country':country})
+    return render(request,'index.html',{'temperature':temperature, 'cityname':cityname, 'country':country, 'wind_speed':wind_speed, 'humidity':humidity, 'weather':weather, 'description':description, 'img_url':img_url})
 
